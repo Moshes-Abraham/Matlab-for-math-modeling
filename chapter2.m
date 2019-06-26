54a0a9b7d702970e1de6adc4f58d697827ea9821
Moshes-Abraham <yoshissac6@gmail.com>
2019-06-26 22:20:09 +0800

    remove a tmp file

--- chapter2.m
+++ chapter2.m
@@ -1,12 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+diary ~/.tmp/matlab-log/outputs
+% The outputs below will be store in a tmp file.
+
 %% test1
 c = [2 3 1]';
 a = [1 4 2;3 2 0];
-b = [8;6];
+b = [8;6]
 [x,y] = linprog(c,-a,-b,[],[],zeros(3,1));
 
+
+
+diary off
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 %% test2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+diary ~/.tmp/matlab-log/outputs
+% The outputs below will be store in a tmp file.
 
 z = [-5;-4;-6];
 a = [1 -1 1;3 2 4;3 2 0];
 b = [20;42;30];
 [x,fval,exitflag,output,lambda] = linprog(z,a,b,[],[],zeros(3,1))
+
+diary off
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
