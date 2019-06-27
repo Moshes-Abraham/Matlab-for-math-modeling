1905ac8a24712b05924f0f73100fcf7232164691
Moshes-Abraham <yoshissac6@gmail.com>
2019-06-27 22:51:25 +0800

    add some comments

---
+++ GA/README.md
@@ -0,0 +1,6 @@
+# Problem to solve
+\[
+max f(x_{1},x_{2}) = 21.5 + x_{1}\sin(4\pi x_{1}) + x_{2}\sin(20\pi x_{2}),\\
+-3.0\leq x_{1}\leq 12.1, \quad 4.1\leq x_{2}\leq 5.8.
+\]
+
--- GA/adapt.m
+++ GA/adapt.m
@@ -3,8 +3,8 @@ function record = adapt(V,f)
   record = zeros(20,1,'double');
   for i = 1:20
     temp1 = V{i};
-    temp2 = temp1(1:18);
-    temp3 = temp1(19:end);
+    temp2 = temp1(1:18);   % Stands for x1
+    temp3 = temp1(19:end);   % Stands for x2
     record(i,1) = f(bin_x(temp2,1),bin_x(temp3,2));
   end
 end
