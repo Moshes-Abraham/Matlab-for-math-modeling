0a29beb72ac2939b661d3d8e48c847767b8934a1
Moshes-Abraham <yoshissac6@gmail.com>
2019-07-16 17:30:11 +0800

    Fixed some warnings

--- chapter2.m
+++ chapter2.m
@@ -2,11 +2,12 @@
 diary ~/.tmp/matlab-log/outputs
 % The outputs below will be store in a tmp file.
 
+% Linear programming
 %% test1
 c = [2 3 1]';
 a = [1 4 2;3 2 0];
-b = [8;6]
-[x,y] = linprog(c,-a,-b,[],[],zeros(3,1));
+b = [8;6];
+[~,y] = linprog(c,-a,-b,[],[],zeros(3,1));
 
 
 
@@ -22,7 +23,7 @@ diary ~/.tmp/matlab-log/outputs
 z = [-5;-4;-6];
 a = [1 -1 1;3 2 4;3 2 0];
 b = [20;42;30];
-[x,fval,exitflag,output,lambda] = linprog(z,a,b,[],[],zeros(3,1))
+[x,fval,exitflag,output,lambda] = linprog(z,a,b,[],[],zeros(3,1));
 
 diary off
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
