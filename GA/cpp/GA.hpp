c9014176e315610da8af2134c91be3bd1d900145
root <root@DESKTOP-DD9RDCF.localdomain>
2019-08-16 17:20:55 +0800

    Update

--- GA/cpp/GA.hpp
+++ GA/cpp/GA.hpp
@@ -1,12 +1,14 @@
 #include <string>
 #include <iostream>
+#include <iterator>
 using namespace std;
 
 #include <Eigen/Eigen>
 #define LEN 33		// 33 bytes
 #define NUM 20		// 20 chromosomes
-
-template<typename T> string toString(const T& t)
+#define PI 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211704
+template<typename T>
+string toString(const T& t)
 {
 	ostringstream oss;
 	oss << t;
@@ -19,6 +21,9 @@ class GA
 	//	int gen; 		// repeat times
 	//	string objFunc; // container for the objective function.
 		vector<string> v;	// group
+		Eigen::ArrayXXd record(NUM,1);
+		Eigen::Array3d maxrec;
+		
 
 	public:
 	//	GA(string func, int n) :objFunc(func), gen(n)
@@ -30,7 +35,12 @@ class GA
 		void InitGroup();
 		string generateChromosome();
 
-
+		int bin2dec(string);
+	//	double objFunc(double, double);
+		void adapt(double (*)(double, double));
+		void maxrecord();
+		double bin_x();
+		
 };
 
 
@@ -61,9 +71,68 @@ void GA::InitGroup()
 	}
 
 	// test
-	for (auto id : v)
-	{
-		cout << id << endl;
-	}
+//	for (auto id : v)
+//	{
+//		cout << id << endl;
+//	}
 
+}
+
+int GA::bin2dec(string s)
+{
+	// TODO: check string
+	int value = 0;	
+
+	for (int i = s.size() - 1, j = 0; i >= 0; --i, ++j)	
+	{
+		if (s[j] == '1')
+			value += static_cast<int>(pow(2,i));
+	}
+//	cout << value << endl;
+	return value;
+}
+
+//Eigen::ArrayXXd GA::adapt(double (* objFunc)(double, double))
+void GA::adapt(double (* objFunc)(double, double))
+{
+	string temp1; 
+	string tmp;
+	vector<string> temp2, temp3;
+
+	for (int i = 0; i != NUM; ++i)
+	{
+		for (int j = 0; j != NUM; ++j)
+		{
+			temp1 = v[j];
+			for (auto it = temp1.cbegin(); it != (temp1.cbegin() + 18); ++it)
+			{
+				tmp += *it;
+			}
+
+			temp2.push_back(tmp);		// stands for x1
+			tmp.clear();
+			for (auto it = (temp1.cbegin() + 18); it != temp1.cend(); ++it)
+			{
+				tmp += *it;
+			}
+
+			temp3.push_back(tmp);		// stands for x2
+			tmp.clear();
+			
+		}
+		// TODO: Use function pointer
+		record(i, 0) = (* objFunc)(bin2dec(temp2[i]),bin2dec(temp3[i]));
+	}
+}
+
+void maxrecord()
+{
+	Eigen::Array3d::Index i;	// Index of m
+	double m = record.maxCoeff(&i);
+	maxrec(0) = m;
+	string temp1 = v[i];
+	string temp2 = temp1.substr(0,18-1);
+	string temp3 = temp1.substr(18);
+	maxrec(1) = bin_x(temp2,1);
+	maxrec(2) = bin_x(temp3,2);
 }
--- GA/cpp/test.cc
+++ GA/cpp/test.cc
@@ -1,6 +1,10 @@
-#include "GA.hpp"
+//#include "GA.hpp"
+#include <cmath>
+#include <Eigen/Eigen>
+#include <iostream>
 using namespace std;
 
+double MyobjFunc(double, double);
 int main()
 {
 //	Eigen::ArrayXd temp;
@@ -17,6 +21,76 @@ int main()
 //		cout << toString(temp(i)) << "\t";
 //	}
 //	cout << endl;
-	GA g;
-	g.InitGroup();
+	//	GA g;
+	//	g.InitGroup();
+//	string s("10010011");
+//	int value = 0.0;	
+//	for (int i = 7, j = 0; i >= 0; --i, ++j)	
+//	{
+//		if (s[j] == '1')
+//			value += static_cast<int>(pow(2,i));
+//	}
+//	cout << value << endl;
+//	cout << s.size() << endl;
+	//		GA g;
+	//		string st("10110011100101");
+	//		g.bin2dec(st);
+//	GA g;
+//	g.InitGroup();
+//	string temp1; 
+//	string tmp;
+//	vector<string> temp2, temp3;
+//	Eigen::ArrayXXd record(NUM,1);
+//
+//	for (int i = 0; i != NUM; ++i)
+//	{
+//		for (int j = 0; j != NUM; ++j)
+//		{
+//			temp1 = g.v[j];
+//			for (auto it = temp1.cbegin(); it != (temp1.cbegin() + 18); ++it)
+//			{
+//				tmp += *it;
+//			}
+//
+//			temp2.push_back(tmp);		// stands for x1
+//			tmp.clear();
+//			for (auto it = (temp1.cbegin() + 18); it != temp1.cend(); ++it)
+//			{
+//				tmp += *it;
+//			}
+//
+//			temp3.push_back(tmp);		// stands for x2
+//			tmp.clear();
+//			
+//		}
+//		record(i, 0) = objFunc(g.bin2dec(temp2[i]),g.bin2dec(temp3[i]));
+//	}
+//
+//	for (int i = 0; i != NUM; ++i)
+//	{
+//		cout << temp2[i] << "\t" << temp3[i];
+//		cout << endl;
+//	}
+//	
+//	cout << "record = " << endl;
+//	cout << record << endl;
+	//	GA g;
+	//	g.InitGroup();		// remember to Init or it'll cause segmentation fault
+	//	Eigen::ArrayXXd record(NUM,1);
+	//	record = g.adapt(MyobjFunc);
+	//	cout << record << endl;
+	Eigen::Array3d v(1,4212,23321), w(3,4,5);
+	//std::cout << v.maxCoeff() << endl; 		// maxCoeff() returns the max value in an array
+	Eigen::Array3d::Index maxRow;
+	double max = v.maxCoeff(&maxRow);		// get the max value and its index (remember it starts from 0)
+	std :: cout << "Max: " << max << " at: " << maxRow << std :: endl;
+	std :: cout << v(2) << std :: endl;
+
 }
+
+
+//double MyobjFunc(double x1, double x2)
+//{
+//	return 21.5 + x1 * sin (4 * PI * x1) + x2 * sin(20 * PI * x2);
+//	//return sin(x1) + sin(x2);
+//}
